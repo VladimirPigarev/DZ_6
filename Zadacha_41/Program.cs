83a336939b9594449a0af3d4f2760f1e6347e63c
VladimirPigarev <109297010+VladimirPigarev@users.noreply.github.com>
2022-08-24 17:54:00 +0300

    написал код для решения задачи № 41

---
+++ Zadacha_41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача № 41. Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел больше 0 ввел пользователь.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите числа через пробел: ");
+int[] array = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x=>int.Parse(x)).ToArray();
+int result = countingNumbers(array);
+WriteLine(result);
+
+int countingNumbers(int[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i] > 0) count++;
+    }
+    return count;
+}
+
+
+
+
